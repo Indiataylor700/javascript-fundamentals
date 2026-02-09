16e63ddffde2a61091c3ad16409443a762a75317
India Taylor <indiataylor700@gmail.com>
2026-02-08 21:39:44 -0600

    Strings lab

---
+++ labs/stringslab.js
@@ -0,0 +1,20 @@
+// Task 1
+// Assigning " This statement has empty spaces " to an assigned variable.
+let empty1 = " This statement has empty spaces " ;
+console.log(empty1);
+
+// a. includes()
+console.log(empty1.includes('stat')); // output is true
+
+// b. at()
+console.log(empty1.at(7)); // output is t
+
+// c. trim()
+console.log(empty1.trim());
+
+// d. toLowerCase
+console.log(empty1.toLowerCase());
+
+// Task 2
+let poem1 = `Jack and Jill went up the hill to fetch a pail of water.\nJack fell down and broke his crown and Jill came\ tumbling after.`;
+console.log(poem1);
