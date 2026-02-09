7cb83afb519a34a3dcfad0fcda74d10a2492b717
India Taylor <indiataylor700@gmail.com>
2026-02-08 21:39:57 -0600

    Numbers lab

---
+++ labs/numberslab.js
@@ -0,0 +1,21 @@
+// Task 1
+let valueA = 34_038;
+let valueB = 45_838.8734;
+let valueC = 0b0110_0010_1011;
+let valueD = 0x174_C0;
+let valueE = -895_612;
+
+console.log(valueA);
+console.log(valueB);
+console.log(valueC);
+console.log(valueD);
+console.log(valueE);
+
+// Task 2
+let bVal = 0b0001_1101;
+let hVal = 0x1D
+let oVal = 0o35
+
+console.log(bVal);
+console.log(hVal);
+console.log(oVal);
