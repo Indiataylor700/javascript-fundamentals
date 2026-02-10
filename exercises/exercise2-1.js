2e135cfe079c0aa423adb0684c2dbf69b2cb9f29
India Taylor <indiataylor700@gmail.com>
2026-02-10 17:46:48 -0600

    Exercise 2.1

---
+++ exercises/exercise2-1.js
@@ -0,0 +1,19 @@
+// Exercise 2.1
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+str1 = String(str1);
+console.log(str1, typeof(str1));
+
+str2 = String(str2);
+console.log(str2, typeof(str2));
+
+console.log(val1, typeof(val1));
+
+console.log(val2, typeof(val2));
+
+myNum = Number(myNum);
+console.log(myNum, typeof(myNum));
