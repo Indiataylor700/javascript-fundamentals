2d66cfbe2c2adbc041bfaba39f02337bb7093bc4
India Taylor <indiataylor700@gmail.com>
2026-02-11 22:55:36 -0600

    Functions lab

---
+++ labs/functionslab.js
@@ -0,0 +1,20 @@
+/*  Functions and Scopes Lab */
+// Task One:
+function oldPhrase(part1, part2) {
+    return part1 + part2;
+}
+let goal1 = 'You win some, you lose some';
+let goal2 = ' You live to fight another day.';
+//console.log(addTogether(goal1, goal2));
+
+// Task Two:
+function newMath(digit3, digit4 = 15) {
+    return digit3 + digit4;
+}
+//console.log(newMath(30));
+//console.log(newMath(30,40));
+
+// Task Three:
+var oldPhrase1 = (part3, part4) => goal1 + goal2;
+
+//console.log(oldPhrase(goal1,goal2));
